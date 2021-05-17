48fe64960707b14d45e655289407577ad10bf063
Daniel Madalitso Phiri <malgamves@gmail.com>
2021-05-17 19:05:48 +0200

    attempt

--- watch-party/nuxt.config.js
+++ watch-party/nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: "server",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
